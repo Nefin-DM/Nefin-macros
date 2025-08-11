07b1ceae02aeb5dbc85bb577c1f39a9fa1affb22
Nefin-DM <Gornetron@gmail.com>
2025-08-11 01:18:18 -0500

    Update Target_Object.js

--- Foundry_Code_Reference/Target_Object.js
+++ Foundry_Code_Reference/Target_Object.js
@@ -23,9 +23,31 @@ const drawingById = canvas.scene.drawings.get("DRAWING_ID_HERE");
 
 /* Target Item */
 const itemByName = token.actor.items.getName('ITEM_NAME_HERE');
+const itemById = game.items.get("ITEM_ID_HERE")
+
+//Target item that is owned by actor
+const target = Array.from(game.user.targets)[0];
+const named = target.actor.name;
+const targetActor = game.actors.getName(named);
+let myItem = actor.items.find(i => i.name === 'ITEM_NAME_HERE');
 
 /* Target Actor */
 const actorById = game.actors.get("ACTOR_ID_HERE");
 
+// Get all actors which are Player Characters
+const pcs = game.actors.filter(actor => actor.isPC)
+
+// Get all actors which are npcs
+const npcs = game.actors.filter(actor => !actor.isPC)
+
 /* Target Active Effect */
 const activeEffectByName = token.actor.effects.getName('ACTIVE_EFFECT_NAME_HERE')
+
+/* Target Scene */
+const sceneByName = game.scene.getName("SCENE_NAME_HERE")
+const sceneById = game.scene.get("SCENE_ID_HERE")
+
+/* Target Journal */
+const journalById = game.journal.get("JOUNRAL_ID_HERE")
+let journalByName = game.journal.getName("JOURNAL_NAME_HERE")
+
